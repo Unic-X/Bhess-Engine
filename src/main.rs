87f4535f888e70a5e0071c729d2571cb472f200e
Unic-X <armansinghkshatri69@gmail.com>
2023-05-24 14:03:35 +0530

    init

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod piece;
+
+
+fn main() {
+    unimplemented!();
+}
---
+++ src/piece.rs
@@ -0,0 +1,37 @@
+pub enum Color{
+    White,
+    Black
+}
+
+
+
+struct Position{
+    x:u32,
+    y:u32
+}
+
+pub struct About{
+    value: u32,
+    position:Position
+}
+
+
+pub enum Piece {
+    Pawn(About),
+    Knight(About),
+    Bishop(About),
+    Rook(About),
+    Queen(About),
+    King(About),
+}
+
+
+impl Piece{
+
+    
+    // add code here
+}
+
+fn main() {
+    
+}
