00d73eb03bb37dc3ec77951d89a7e1bb7e8748ec
Unic-X <armansinghkshatri69@gmail.com>
2023-10-13 13:19:22 +0530

    King Masking+Testing

--- src/piece.rs
+++ src/piece.rs
@@ -81,3 +81,38 @@ pub fn mask_knight(sq: &[Squares], mut bitboard: u64) -> u64 {
     };
     return attacks;
 }
+
+pub fn mask_king(sq: &[Squares], mut bitboard: u64) -> u64 {
+    let mut attacks: u64 = 0;
+
+    set_bit!(sq, mut bitboard);
+
+    if (bitboard >> 8) > 0 {
+        attacks |= bitboard >> 8
+    };
+
+    if ((bitboard >> 9) & NOT_H_FILE) > 0 {
+        attacks |= bitboard >> 9
+    };
+    if ((bitboard >> 7) & NOT_A_FILE) > 0 {
+        attacks |= bitboard >> 7
+    };
+    if ((bitboard >> 1) & NOT_H_FILE) > 0 {
+        attacks |= bitboard >> 1
+    };
+
+    if (bitboard << 8) > 0 {
+        attacks |= bitboard << 8
+    };
+
+    if ((bitboard << 9) & NOT_A_FILE) > 0 {
+        attacks |= bitboard << 9
+    };
+    if ((bitboard << 7) & NOT_H_FILE) > 0 {
+        attacks |= bitboard << 7
+    };
+    if ((bitboard << 1) & NOT_A_FILE) > 0 {
+        attacks |= bitboard << 1
+    };
+    return attacks;
+}
--- tests/masking.rs
+++ tests/masking.rs
@@ -1,4 +1,4 @@
-use samaj::piece::{Sides,mask_pawn, mask_knight};
+use samaj::piece::{Sides,mask_pawn, mask_knight, mask_king};
 use samaj::board::{Squares, render};
 
 #[test]
@@ -10,4 +10,10 @@ fn test_pawn_masking() {
 fn test_knight_masking() {
     render(mask_knight(&[Squares::d5],0));
     assert_eq!( mask_knight(&[Squares::e3], 68719476736),8796093022208);
+}
+
+#[test]
+fn test_king_masking() {
+    render(mask_king(&[Squares::h1],0));
+    assert_eq!( mask_king(&[Squares::e3], 68719476736),8796093022208);
 }
