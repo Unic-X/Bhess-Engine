e76b71c2255f3f3974c65587697b8be8b5aba3a5
Unic-X <armansinghkshatri69@gmail.com>
2023-11-03 02:11:25 +0530

    Formatting fix

--- src/utils.rs
+++ src/utils.rs
@@ -24,11 +24,31 @@ pub fn bit_count(bitboard:u64)->u64{
 
 }
 
+const BISHOP_RELEVANT_BITS: [u8;64] = [
+    6, 5, 5, 5, 5, 5, 5, 6, 
+    5, 5, 5, 5, 5, 5, 5, 5, 
+    5, 5, 7, 7, 7, 7, 5, 5, 
+    5, 5, 7, 9, 9, 7, 5, 5, 
+    5, 5, 7, 9, 9, 7, 5, 5, 
+    5, 5, 7, 7, 7, 7, 5, 5, 
+    5, 5, 5, 5, 5, 5, 5, 5, 
+    6, 5, 5, 5, 5, 5, 5, 6
+];
+
+// rook relevant occupancy bit count for every square on board
+const ROOK_REVEVANT_BITS: [u8;64] = [
+    12, 11, 11, 11, 11, 11, 11, 12, 
+    11, 10, 10, 10, 10, 10, 10, 11, 
+    11, 10, 10, 10, 10, 10, 10, 11, 
+    11, 10, 10, 10, 10, 10, 10, 11, 
+    11, 10, 10, 10, 10, 10, 10, 11, 
+    11, 10, 10, 10, 10, 10, 10, 11, 
+    11, 10, 10, 10, 10, 10, 10, 11, 
+    12, 11, 11, 11, 11, 11, 11, 12
+];
 
 
-
-
-fn set_occupancy(index: u64, bits_in_mask: u32, attack_mask: u64) -> u64 {
+pub fn set_occupancy(index: u64, bits_in_mask: u32, attack_mask: u64) -> u64 {
     let mut occupancy: u64 = 0;
     let mut current_attack_mask = attack_mask;
 
